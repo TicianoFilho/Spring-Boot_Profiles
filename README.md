12f234647c83ed8a17c1f56f9fc28fb1452769a4
TicianoFilho <ticianofilho@gmail.com>
2022-04-24 13:41:43 -0300

    My first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Trabalhando com Profiles no Spring Boot
+
+##Projeto desenvolvido na aula do Carrefour Bootcamp para aprender a trabalhar com Profiles no Spring Boot. Neste projeto foram criados dois profiles: Desenvolvimento e Produção.
---
+++ src/main/java/com/digitalinovationone/springbootconfig/config/DbConfiguration.java
@@ -0,0 +1,39 @@
+package com.digitalinovationone.springbootconfig.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")              //Will map all properties beginning with "spring.datasource"
+@Getter
+@Setter
+public class DbConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")                                                         //Wil map "application.dev.properties" file
+    @Bean
+    public String testDataBaseConnection(){
+        System.out.println("DB Connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to H2_TEST - Test instance";
+    }
+
+    @Profile("prod")                                                      //will map "application.prod.properties" file
+    @Bean
+    public String ProductionDataBaseConnection() {
+        System.out.println("DB Connection for Production - MYSQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_PROD - Production instance";
+    }
+
+}
